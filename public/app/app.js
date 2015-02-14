dd575d410a3f80d8a816029a875d8183d8aa016b
Haddon Korinek <haddonkorinek@gmail.com>
2015-02-13 21:26:12 -0600

    Set up mocha/chai to run in a browser and added DS_Store to the git ignore.

--- public/app/app.js
+++ public/app/app.js
@@ -1,12 +1,12 @@
 'use strict';
 
 angular.module('app', ['ngResource', 'ngRoute']);
-angular.module('app').config(function($routeProvider, $locationProvider) {
-	$locationProvider.html5Mode = true;
-	$routeProvider
-      .when('/', 
-      	{
-					templateUrl: '/partials/main',
-        	controller: 'mainController'
-      	});
+angular.module('app').config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode = true;
+    $routeProvider
+        .when('/',
+        {
+            templateUrl: '/partials/main',
+            controller: 'mainController'
+        });
 });
