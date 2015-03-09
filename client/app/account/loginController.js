f73fab1302854f809391d6d4f9e1a69ca9433d47
Haddon Korinek <haddonkorinek@gmail.com>
2015-03-09 12:54:44 -0500

    Moved the navbar into a directive.

---
+++ client/app/account/loginController.js
@@ -0,0 +1,7 @@
+(function () {
+    'use strict';
+
+    angular.module('app').controller('loginController', function () {
+        var vm = this;
+    });
+}());
---
+++ client/app/navbar/navbarDirective.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+
+    angular.module('app').directive('navbarDirective', function () {
+        return {
+            templateUrl: '/app/navbar/navbarTemplate.html'
+        };
+    });
+}());
