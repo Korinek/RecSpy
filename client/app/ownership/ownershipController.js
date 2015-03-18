dd9ac7116c7919d1bbabd9f0d1852024bf8cefbe
Haddon Korinek <haddonkorinek@gmail.com>
2015-03-17 20:29:33 -0500

    Fixed boostrap columns in signup.html. Added some code for testing purposes to display whether or not the user owns a gym or not.

--- client/app/ownership/ownershipController.js
+++ client/app/ownership/ownershipController.js
@@ -1,16 +1,18 @@
 (function() {
     'use strict';
 
-    var OwnershipController = function(ownershipService) {
+    var OwnershipController = function(ownershipService, requestErrorService) {
         var vm = this;
         ownershipService.getOwnedGym().then(function(success, gym) {
-            if(success) {
-                console.log('getOwnedGym success');
+            if (success) {
                 console.log(gym);
+                vm.gym = gym;
+            } else {
+                requestErrorService.handleSessionExpired();
             }
         });
     };
 
-    OwnershipController.$inject = ['ownershipService'];
+    OwnershipController.$inject = ['ownershipService', 'requestErrorService'];
     angular.module('app').controller('OwnershipController', OwnershipController);
 }());
