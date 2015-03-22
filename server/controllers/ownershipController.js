753cb4a78417e6b6237d55a3bd84f10caaf4503b
Haddon Korinek <haddonkorinek@gmail.com>
2015-03-21 21:45:36 -0500

    The ownership page will now display first/last names of all users. Took a bit of callback hell to get it done. Will definitely need to refactor this.

--- server/controllers/ownershipController.js
+++ server/controllers/ownershipController.js
@@ -1,4 +1,30 @@
-var Gym = require('mongoose').model('Gym');
+var mongoose = require('mongoose'),
+    Gym = mongoose.model('Gym'),
+    User = mongoose.model('User'),
+    async = require('async');
+
+var getRealName = function(userId, newUserList, next) {
+    User.findOne({
+        _id: userId
+    }, function(err, user) {
+        console.log(user.firstName + ' ' + user.lastName);
+
+        newUserList.push({
+            firstName: user.firstName,
+            lastName: user.lastName
+        });
+        next();
+    });
+};
+
+var convertUserIdListToNames = function(userIdList, next) {
+    var newUserList = [];
+    async.each(userIdList, function(userid, callback) {
+        getRealName(userid, newUserList, callback);
+    }, function(err) {
+        next(newUserList);
+    });
+};
 
 exports.getOwnership = function(req, res, next) {
     Gym.findOne({
@@ -9,9 +35,36 @@ exports.getOwnership = function(req, res, next) {
             res.send({
                 reason: err.toString()
             });
-        } else {
-            res.send(gym);
         }
+
+        var scrubbedGym = {
+            name: gym.name
+        };
+
+        convertUserIdListToNames(gym.employees, function(employees) {
+            scrubbedGym.employees = employees;
+            console.log('finished employees');
+
+            convertUserIdListToNames(gym.members, function(members) {
+                scrubbedGym.members = members;
+                console.log('finsihed members');
+
+                convertUserIdListToNames(gym.pendingMembers, function(pendingMembers) {
+                    scrubbedGym.pendingMembers = pendingMembers;
+                    console.log('finished pending members');
+                    console.log(pendingMembers);
+
+                    convertUserIdListToNames(gym.pendingEmployees, function(pendingEmployees) {
+                        scrubbedGym.pendingEmployees = pendingEmployees;
+                        console.log('finished pending employees');
+
+                        console.log('--sending scrubbed gym--');
+                        console.log(scrubbedGym);
+                        res.send(scrubbedGym);
+                    });
+                });
+            });
+        });
     });
 };
 
